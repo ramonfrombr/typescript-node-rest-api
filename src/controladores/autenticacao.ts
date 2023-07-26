95199ff536f86b2c87c223a32c031b4fdeeab194
ramonfrombr@gmail.com <ramonfrombr>
2023-07-26 19:45:34 -0300

    06. controlador autenticacao

---
+++ src/controladores/autenticacao.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import { criarUsuario, selecionarUsuarioPeloEmail } from '../db/usuarios';
+import { aleatorio, autenticacao } from '../auxiliares';
+
+export const register = async (pedido: express.Request, resposta: express.Response) => {
+    try {
+        const {email, senha, nome_usuario} = pedido.body;
+
+        if (!email || !senha || !nome_usuario) {
+            return resposta.sendStatus(400);
+        }
+
+        const usuarioExistente = await selecionarUsuarioPeloEmail(email);
+
+        if(usuarioExistente) {
+            return resposta.sendStatus(400);
+        }
+
+        const salt = aleatorio();
+
+        const usuario = await criarUsuario({
+            email,
+            nome_usuario,
+            autenticacao: {
+                salt,
+                senha: autenticacao(salt, senha)
+            }
+        });
+
+        return resposta.status(200).json(usuario).end();
+    } catch(erro) {
+        console.log(erro);
+        return resposta.sendStatus(400);
+    }
+}
