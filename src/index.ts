1928a59da4c46a2041a3ea64a736e1f93b04b12e
ramonfrombr@gmail.com <ramonfrombr>
2023-07-26 17:24:39 -0300

    02. configuração express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Olá, TypeScript");
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({
+    credentials: true,
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const servidor  = http.createServer(app);
+
+servidor.listen(8080, () => {
+    console.log("Servidor rodando no endereço http://localhost:8080/")
+})
